f7bfc27ea5b751465cd02d4b78933ec8323d65d3
cabala191 <cabala191@gmail.com>
2016-04-13 19:33:54 +0200

    Update game.c

--- game.c
+++ game.c
@@ -2,10 +2,7 @@
 
 int main()
 {
-<<<<<<< HEAD
-=======
->>>>>>> 63b992459fc8ef3acff3b7404863a1d31cfb8d53
->>>>>>> 63b992459fc8ef3acff3b7404863a1d31cfb8d53
+
 ;
 ;
 printf("staszek mówi szczerą prawdę!!!!")
