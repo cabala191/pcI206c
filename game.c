caca9c6377a57db2bd11f2656a0bf52b750f7ea1
Ewastachow <ewa.sss.ewa@gmail.com>
2016-04-13 19:18:39 +0200

    game

--- game.c
+++ game.c
@@ -3,9 +3,10 @@
 int main()
 {
 <<<<<<< HEAD
-    printf("klamstwo");
 =======
     printf("Maciej to NIE JEST lewacki aktywista");
 >>>>>>> 63b992459fc8ef3acff3b7404863a1d31cfb8d53
+
+printf("staszek klamie!!!!")
     return 0;
 }
