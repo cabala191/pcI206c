3b29ba2c4354fc542930866d8148afb9f77c9fef
cabala191 <cabala191@gmail.com>
2016-04-13 19:27:17 +0200

    Update game.c

--- game.c
+++ game.c
@@ -4,9 +4,9 @@ int main()
 {
 <<<<<<< HEAD
 =======
-    printf("Maciej to NIE JEST lewacki aktywista");
+    printf("Maciej to lewacki aktywista");
 >>>>>>> 63b992459fc8ef3acff3b7404863a1d31cfb8d53
 ;
-printf("staszek klamie!!!!")
+printf("staszek mówi szczerą prawdę!!!!")
     return 0;
 }
