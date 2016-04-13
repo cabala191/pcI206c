59ab6a661a171089411b2df479fb7d8825b82450
aolak <aolak.agh@gmail.com>
2016-04-13 19:14:39 +0200

    Added files via upload

---
+++ trzeci.c
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+#include <stdio.h>
+
+int main(){
+    int i=2;
+    printf("%d", i);
+}
+=======
+#include <stdio.h>
+
+int main(){
+    int i=2;
+    printf("%d", i);
+}
+>>>>>>> a87ac19cdc53f52a34c15b3323d2e6cdc73213d1
